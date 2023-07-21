38c988cd69326fbd8fdc3f69be2b9b0d44c09eb8
Neander de Souza <neander.faria@hotmail.com>
2023-07-21 10:35:33 -0300

    Conectando à API - Criando interface de loading

--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,4 +1,4 @@
-import { PayloadAction, createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 import { api } from "../../lib/axios";
 
@@ -21,22 +21,21 @@ export interface PlayerState {
   course: Course | null;
   currentModuleIndex: number;
   currentLessonIndex: number;
+  isLoading: boolean;
 }
 
 const initialState: PlayerState = {
   course: null,
   currentModuleIndex: 0,
   currentLessonIndex: 0,
+  isLoading: false,
 };
 
-export const loadCourse = createAsyncThunk(
-  'player/load',
-  async () => {
-    const response = await api.get('/courses/1')
+export const loadCourse = createAsyncThunk("player/load", async () => {
+  const response = await api.get("/courses/1");
 
-    return response.data
-  }
-)
+  return response.data;
+});
 
 export const playerSlice = createSlice({
   name: "player",
@@ -67,9 +66,13 @@ export const playerSlice = createSlice({
     },
   },
   extraReducers(builder) {
+    builder.addCase(loadCourse.pending, (state) => {
+      state.isLoading = true;
+    });
     builder.addCase(loadCourse.fulfilled, (state, action) => {
-      state.course = action.payload
-    })
+      state.course = action.payload;
+      state.isLoading = false;
+    });
   },
 });
 
