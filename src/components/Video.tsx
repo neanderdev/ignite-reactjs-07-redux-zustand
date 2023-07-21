d10840b0badeb6ae56bddc91b62e9f8a0e5c330c
Neander de Souza <neander.faria@hotmail.com>
2023-07-21 10:08:25 -0300

    Aplicação com Redux - Configurando autoplay

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,8 +1,13 @@
 import ReactPlayer from 'react-player';
+import { useDispatch } from "react-redux";
+
+import { next } from "../store/slices/player";
 
 import { useAppSelector } from "../store";
 
 export function Video() {
+    const dispatch = useDispatch()
+
     const lesson = useAppSelector(state => {
         const { currentModuleIndex, currentLessonIndex } = state.player
 
@@ -12,12 +17,18 @@ export function Video() {
         return currentLesson
     })
 
+    function handlePlayNext() {
+        dispatch(next())
+    }
+
     return (
         <div className="w-full bg-zinc-950 aspect-video">
             <ReactPlayer
                 width="100%"
                 height="100%"
                 controls
+                playing
+                onEnded={handlePlayNext}
                 url={`https://www.youtube.com/watch?v=${lesson.id}`}
             />
         </div>
