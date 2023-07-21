0014d1befb3de654362c0a3b344c3f4bc02d0f21
Neander de Souza <neander.faria@hotmail.com>
2023-07-21 10:14:10 -0300

    Aplicação com Redux - Criando hook global

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,8 +1,11 @@
 import { MessageCircle } from "lucide-react";
+import { useEffect } from "react";
 
 import { Header } from "../components/Header";
 import { Module } from "../components/Module";
 import { Video } from "../components/Video";
+
+import { useCurrentLesson } from "../store/slices/player";
 
 import { useAppSelector } from "../store";
 
@@ -10,6 +13,12 @@ export function Player() {
     const modules = useAppSelector(state => {
         return state.player.course.modules
     })
+
+    const { currentLesson } = useCurrentLesson()
+
+    useEffect(() => {
+        document.title = `Assistindo: ${currentLesson.title}`
+    }, [currentLesson]);
 
     return (
         <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,5 +1,7 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
+import { useAppSelector } from "..";
+
 const playerSlice = createSlice({
   name: "player",
   initialState: {
@@ -88,3 +90,14 @@ const playerSlice = createSlice({
 export const player = playerSlice.reducer;
 
 export const { play, next } = playerSlice.actions;
+
+export const useCurrentLesson = () => {
+  return useAppSelector((state) => {
+    const { currentModuleIndex, currentLessonIndex } = state.player;
+
+    const currentModule = state.player.course.modules[currentModuleIndex];
+    const currentLesson = currentModule.lessons[currentLessonIndex];
+
+    return { currentModule, currentLesson };
+  });
+};
